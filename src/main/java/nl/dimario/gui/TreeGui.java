edf977165987d1642401c2e2a9d1b2b2550ba4f5
Mario Pinkster <mario.pinkster@indivirtual.com>
2019-12-09 11:19:11 +0100

    Removed "nimus" look and feel

--- src/main/java/nl/dimario/gui/TreeGui.java
+++ src/main/java/nl/dimario/gui/TreeGui.java
@@ -158,12 +158,6 @@ public class TreeGui extends JFrame {
     public static void main(String args[]) {
 
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
             TreeGui gui = new TreeGui();
             gui.buildGui();
             if( args.length > 0) {
